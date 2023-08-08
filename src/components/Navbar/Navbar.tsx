d3afdd863dbb3d1711c6c0c520bd9361dee9d38f
Tomas <tomasmoscarelli@hotmail.com>
2023-08-08 03:59:22 -0300

    added navbar and auth page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 
 type NavbarProps = {
@@ -6,6 +8,14 @@ type NavbarProps = {
 
 const Navbar:React.FC<NavbarProps> = () => {
     
-    return <div>Navbar</div>
+    return <div className='flex items-center justify-between h-20 sm:px-12 px-4 md:px-24'>
+        <Link href='/' className='flex items-center justify-center'>
+            <Image src="/logo-full.png" width='120' height='120' alt='LeetClone' />
+        </Link>
+        <div className='flex items-center'>
+            <button className='bg-brand-orange text-white rounded-3xl h-9 px-6 text-sm outline-none
+            hover:bg-brand-orange-s transition duration-300 ease-in-out'>Log In</button>
+        </div>
+    </div>
 }
 export default Navbar;
