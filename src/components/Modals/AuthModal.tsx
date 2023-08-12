69ea2de7334ec7594ef32414370af276937831a5
Tomas <tomasmoscarelli@hotmail.com>
2023-08-12 05:30:36 -0300

    fix modal auth responsive

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -18,14 +18,18 @@ const AuthModal:React.FC<AuthModalProps> = () => {
     }
     return <>
         <div className='absolute h-full w-full top-0 left-0 flex justify-center items-center bg-black opacity-60 overlay-enter' onClick={handleCloseModal}></div>
-        <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col w-96 bg-brand-orange rounded-lg outline-none mx-5 modal-enter'>
-            <div className='flex w-full justify-end'>
-                <button onClick={handleCloseModal} className='flex justify-center items-center rounded-full h-6 w-6 hover:bg-zinc-800
-                    text-white mx-1 my-1 transition duration-300 ease-in-out'>
-                    <IoClose />
-                </button>
+        <div className='w-full sm:w-[450px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-centers'>
+            <div className='relative w-full h-full mx-auto flex items-center justify-center'>
+                <div className='relative bg-brand-orange rounded-lg outline-none mx-5 w-96 modal-enter'>
+                    <div className='flex w-full justify-end'>
+                        <button onClick={handleCloseModal} className='flex justify-center items-center rounded-full h-6 w-6 hover:bg-zinc-800
+                            text-white mx-1 my-1 transition duration-300 ease-in-out'>
+                            <IoClose />
+                        </button>
+                    </div>
+                    {authModal.type === 'login' ? <Login/> : authModal.type === 'register' ? <Signup/> : <ResetPassword/>}
+                </div>
             </div>
-            {authModal.type === 'login' ? <Login/> : authModal.type === 'register' ? <Signup/> : <ResetPassword/>}
         </div>
     </>
 }
