b4e2d899baabd3d26270ee8e2f3e7acb796f6c16
Tomas <tomasmoscarelli@hotmail.com>
2023-08-11 23:59:12 -0300

    added firebase config

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,5 +1,6 @@
 import { authModalState } from '@/atoms/authModalAtom';
 import React from 'react';
+import { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 
 type SignupProps = {
@@ -11,20 +12,29 @@ const Signup:React.FC<SignupProps> = () => {
     function handleLoginModal() {
         setLoginModalState(prev => ({...prev, type: 'login'}))
     }
-    return <form className='mt-3 px-8 pb-4'>
+    const [inputs, setInputs] = useState({email: '', displayName: '', password: ''});
+
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    function handleRegister(e: React.FormEvent<HTMLFormElement>) {
+    }
+
+    return <form className='mt-3 px-8 pb-4' onSubmit={handleRegister}>
         <h3 className='text-2xl text-white font-medium'>Register to <span className='text-zinc-800 font-bold'>LeetClone</span></h3>
         <div>
             <div className='mt-4'>
                 <label htmlFor='email' className='text-zinc-800 font-light text-sm block'>Email</label>
-                <input className='rounded-md w-full h-9 mt-1 p-2.5 text-sm outline-none bg-zinc-700 text-white placeholder-zinc-400' type='email' name='email' id='email' placeholder='johndoe@gmail.com' autoComplete='off'/>
+                <input onChange={handleInputChange} className='rounded-md w-full h-9 mt-1 p-2.5 text-sm outline-none bg-zinc-700 text-white placeholder-zinc-400' type='email' name='email' id='email' placeholder='johndoe@gmail.com' autoComplete='off'/>
             </div>
             <div className='mt-4'>
                 <label htmlFor='displayName' className='text-zinc-800 font-light text-sm block'>Display Name</label>
-                <input className='rounded-md w-full h-9 mt-1 p-2.5 text-sm outline-none bg-zinc-700 text-white placeholder-zinc-400' type='displayName' name='displayName' id='displayName' placeholder='John Doe' autoComplete='off'/>
+                <input onChange={handleInputChange} className='rounded-md w-full h-9 mt-1 p-2.5 text-sm outline-none bg-zinc-700 text-white placeholder-zinc-400' type='displayName' name='displayName' id='displayName' placeholder='John Doe' autoComplete='off'/>
             </div>
             <div className='mt-4'>
                 <label htmlFor='password' className='text-zinc-800 font-light text-sm block'>Password</label>
-                <input className='rounded-md w-full h-9 mt-1 p-2.5 text-sm outline-none bg-zinc-700 text-white placeholder-zinc-400' type='password' name='password' id='password' placeholder='password' autoComplete='off'/>
+                <input onChange={handleInputChange} className='rounded-md w-full h-9 mt-1 p-2.5 text-sm outline-none bg-zinc-700 text-white placeholder-zinc-400' type='password' name='password' id='password' placeholder='password' autoComplete='off'/>
             </div>
             <button type='submit' className='w-full bg-zinc-800 text-white text-sm rounded-md h-9 mt-4 block hover:bg-zinc-900 transition duration-300 ease-in-out'>Register</button>
             <div className='mt-3'>
