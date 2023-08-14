ee4e33f2c17a789fb871e688872805dd84a06d9b
Tomas <tomasmoscarelli@hotmail.com>
2023-08-14 05:39:27 -0300

    added auth page route guard

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,11 @@
 import Navbar from '@/components/Navbar/Navbar';
 import AuthModal from '@/components/Modals/AuthModal';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { authModalState } from '@/atoms/authModalAtom';
 import { useRecoilValue } from 'recoil';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/firebase';
+import { useRouter } from 'next/router';
 
 type AuthPageProps = {
     
@@ -10,6 +13,17 @@ type AuthPageProps = {
 
 const AuthPage:React.FC<AuthPageProps> = () => {
     const authModal = useRecoilValue(authModalState);
+    const [user, loading, error] = useAuthState(auth);
+    const [pageLoading, setPageLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (user) router.push('/')
+        if (!loading && !user) setPageLoading(false);
+    }, [user, router, loading])
+
+    if (pageLoading) return null;
+
     return <div className='bg-zinc-800 h-screen relative'>
         <div className='max-w-7x1 mx-auto'>
             <Navbar />
